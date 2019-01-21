adcfec5c561e20a3550c964320b84885f44b7ca5
wmid1 <34446943+wmid1@users.noreply.github.com>
2019-01-21 12:23:53 +0600

    Fix link

--- src/api.js
+++ src/api.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
 
-const AUTH_API_URL = 'https://floating-atoll-63112.herokuapp.com';
+const BASE_API_URL = 'https://floating-atoll-63112.herokuapp.com';
+const AUTH_API_URL = `${BASE_API_URL}/api/auth`;
 
 export function fetchSignIn(email, password) {
-  return axios.post(`${AUTH_API_URL}/api/auth/sign_in`, {
+  return axios.post(`${AUTH_API_URL}/sign_in`, {
     email,
     password,
   });
 }
 
 export function fetchRegister(firstName, lastName, email, password) {
-  return axios.post(`${AUTH_API_URL}/api/auth`, {
+  return axios.post(AUTH_API_URL, {
     first_name: firstName,
     last_name: lastName,
     email,
     password,
     config_name: 'default',
-    confirm_success_url: AUTH_API_URL,
+    confirm_success_url: BASE_API_URL,
   });
 }
 
-export function validation(userDate) {
-  return axios.get(`${AUTH_API_URL}/api/auth/validate_token`, { headers: userDate });
+export function validation(userData) {
+  return axios.get(`${AUTH_API_URL}/validate_token`, { headers: userData });
 }
 
-export function logOut(userDate) {
-  return axios.delete(`${AUTH_API_URL}/api/auth/sign_out`, { headers: userDate });
+export function logOut(userData) {
+  return axios.delete(`${AUTH_API_URL}/sign_out`, { headers: userData });
 }
