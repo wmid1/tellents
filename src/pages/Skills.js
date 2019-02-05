4556d6188c2c107c23dcddedf6c26060115d9920
wmid1 <34446943+wmid1@users.noreply.github.com>
2019-02-06 01:33:57 +0600

    Move ul from pages\Skills to skillBox\SkillLinks
    Implementation of links through func map()

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,19 +1,11 @@
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../components/header/Header';
-import SkillBox from '../components/SkillBox';
+import SkillBox from '../components/skillBox/SkillBox';
+import SkillLinks from '../components/skillBox/SkillLinks';
 import '../styles/styles.css';
 
 class Skills extends Component {
-  state = {
-    value: '',
-    currentWindow: 'skills',
-  };
-
-  setWindow = currentWindow => {
-    this.setState({ currentWindow });
-  };
-
   render() {
     return (
       <div className="startWindow">
@@ -24,64 +16,8 @@ class Skills extends Component {
               <div className="panel-body">
                 <div className="flexbox">
                   <div className="right-col">
-                    <ul className="nav nav-pills nav-stacked my-sidebar" role="tablist">
-                      <li className={this.state.currentWindow === 'overview' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('overview')}>
-                          <span className="icon icon-overview" />
-                          Overview
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'messages' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('messages')}>
-                          <span className="icon icon-message" />
-                          Messages
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'my-jobs' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('my-jobs')}>
-                          <span className="icon icon-jobs" />
-                          My Jobs
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'my-promo' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('my-promo')}>
-                          <span className="icon icon-promo" />
-                          My Promo
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'skills' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('skills')}>
-                          <span className="icon icon-skills" />
-                          My Skills
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'saved' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('saved')}>
-                          <span className="icon icon-saved" />
-                          Saved
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'media' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('media')}>
-                          <span className="icon icon-media" />
-                          My Media
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'accounts' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('accounts')}>
-                          <span className="icon icon-accounts" />
-                          Accounts
-                        </Link>
-                      </li>
-                      <li className={this.state.currentWindow === 'profile' ? 'active' : ''}>
-                        <Link to="#" onClick={() => this.setWindow('profile')}>
-                          <span className="icon icon-profile" />
-                          Profile
-                        </Link>
-                      </li>
-                    </ul>
+                    <SkillLinks />
                   </div>
-
                   <div className="left-col">
                     <SkillBox />
                   </div>
